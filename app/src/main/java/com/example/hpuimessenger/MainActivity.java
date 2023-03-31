608199b1a5faa6cc3a9dc6530c013eb89bc3c025
Ahmed Shariff <shariff.mfa@outlook.com>
2023-03-30 17:14:42 -0700

    Setting up hpui messages

--- app/src/main/java/com/example/hpuimessenger/MainActivity.java
+++ app/src/main/java/com/example/hpuimessenger/MainActivity.java
@@ -15,24 +15,37 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private MqttAndroidClient client;
-
+    private Map<String, Integer> targets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        targets = new HashMap<String, Integer>() {{
+                put("index_distal", R.id.index_distal);
+                put("index_middle", R.id.index_middle);
+                put("index_proximal", R.id.index_proximal);
+                put("middle_distal", R.id.middle_distal);
+                put("middle_middle", R.id.middle_middle);
+                put("middle_proximal", R.id.middle_proximal);
+                put("ring_distal", R.id.ring_distal);
+                put("ring_middle", R.id.ring_middle);
+                put("ring_proximal", R.id.ring_proximal);
+                put("pinky_distal", R.id.pinky_distal);
+                put("pinky_middle", R.id.pinky_middle);
+                put("pinky_proximal", R.id.pinky_proximal);
+        }};
+
         String server = "ws://192.168.1.64:80";
         String clientId = "Android_HPUI_Client";
-        String topic = "Messaging";
+        String topic = "hpui/#";
 
         client = new MqttAndroidClient(this, server, clientId);
 
@@ -46,9 +59,19 @@ public class MainActivity extends AppCompatActivity {
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 Log.d("MQTT", "Received message: " + message.toString());
 
+                String subTopic = topic.split("/")[1];
+                for (Map.Entry<String, Integer> entry : targets.entrySet()) {
+                    TextView textView = findViewById(entry.getValue());
+                    if (entry.getKey().equalsIgnoreCase(subTopic)) {
+                        textView.setText(message.toString());
+                    } else {
+                        textView.setText("");
+                    }
+                }
+
                 runOnUiThread(() -> {
                     TextView textView = findViewById(R.id.fullMsg);
-                    textView.setText(message.toString());
+                    textView.setText("TOPIC: " + topic+"   MESSAGE:"+ message.toString());
                 });
             }
 
